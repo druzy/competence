df898e74e6460fdb678352119de7626fb6c58853
druzy <jonathan.legreneur@free.fr>
2017-01-12 23:58:30 +0100

    rm .rb

--- lib/about_view.rb
+++ lib/about_view.rb
@@ -10,25 +10,26 @@ JRuby.objectspace = true
 module Competence
 
   class AboutApp < JRubyFX::Application
-    
+
     attr_accessor :view, :main_thread
 
     def start(stage)
       stage.title = 'À propos de Compétence'
+      puts 'file:'+@view.controller.model.icon
       stage.icons.add(Image.new('file:'+@view.controller.model.icon))
       stage.resizable = false
 
       controller  = stage.fxml(AboutAppController)
       controller.view = @view
       @view.controller_app = controller
-      
+
       stage.show
     end
 
   end
 
   class AboutAppController
-    
+
     attr_accessor :view, :stage, :field
 
     include JRubyFX::Controller
@@ -41,9 +42,9 @@ module Competence
     end
 
   end
-  
+
   class AboutView < Druzy::MVC::View
-    
+
     attr_accessor :controller_app
 
     java_import javafx.application.Platform
@@ -54,12 +55,12 @@ module Competence
 
       Platform.run_later do
         @stage = javafx.stage.Stage.new
-        
-	app = AboutApp.new
-	app.view = self
-	app.main_thread = Thread.current
-	
-	app.start(@stage)
+
+	      app = AboutApp.new
+        app.view = self
+        app.main_thread = Thread.current
+
+	      app.start(@stage)
       end
 
     end
@@ -78,4 +79,3 @@ module Competence
 
   end
 end
-
--- lib/analyze_view.rb
+++ lib/analyze_view.rb
@@ -5,31 +5,30 @@ require 'jrubyfx'
 fxml_root File.dirname(__FILE__)
 
 module Competence
-  
-  class AnalyzeApp < JRubyFX::Application    
+
+  class AnalyzeApp < JRubyFX::Application
   end
-	
+
   class AnalyzeAppController
-	include JRubyFX::Controller
-        fxml('about_view.fxml')
-	
+    include JRubyFX::Controller
+    fxml('about_view.fxml')
+
   end
+
   class AnalyzeView < Druzy::MVC::View
-    
-    
+
+
 
     def initialize(controller)
       super(controller)
 
-      
+
     end
 
     def display
-      @analyze_frame.visible = true
     end
 
     def close
-      @analyze_frame.dispose
     end
   end
 end
